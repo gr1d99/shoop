81f2c8a8f983e57e2855c66519717f5ef9b2bdad
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2023-04-20 17:03:29 +0300

    chore: revamp deployment scripts

--- deployment/functions.sh
+++ deployment/functions.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
 before_install() {
-  sudo rm -rf /home/shoop/shoop
+    sudo rm -rf /home/shoop
 }
 
 after_install() {
-    cd /home/shoop/shoop
-
-    # nginx config
-    sudo touch /etc/nginx/sites-enabled/shoop.conf
-    sudo touch /etc/nginx/conf.d/shoop.conf
-    sudo cp /home/shoop/shoop/deployment/shoop.nginx.conf /etc/nginx/sites-enabled/shoop.conf
+    sudo cp /home/shoop/deployment/shoop_nginx.conf /etc/nginx/sites-available/shoop.conf
+    if [ -f "/etc/nginx/sites-enabled/shoop.conf" ]
+    then
+      sudo unlink /etc/nginx/sites-enabled/shoop.conf
+      sudo ln -s /etc/nginx/sites-available/shoop.conf /etc/nginx/sites-enabled/shoop.conf
+    else
+      sudo ln -s /etc/nginx/sites-available/shoop.conf /etc/nginx/sites-enabled/shoop.conf
+    fi
 }
 
 application_start() {
-    sudo -i -u shoop << EOF
-    rvm install 3.2.0
-    rvm use 3.2.0 --default
-    rvm autolibs disable
-
-    cd /home/shoop/shoop
-    bundle install
-    bundle exec rails db:prepare
-EOF
+#    sudo -i -u shoop << EOF
+#    rvm install 3.2.0
+#    rvm use 3.2.0 --default
+#    rvm autolibs disable
+#
+#    cd /home/shoop/shoop
+#    bundle install
+#    bundle exec rails db:prepare
+#EOF
     sudo service nginx start
 }
 
